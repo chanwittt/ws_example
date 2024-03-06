9a5072136f419224053232f1251574e28e9d04f9
Chanwit Sangthong <chanwittt@gmail.com>
2024-03-06 12:58:03 +0700

    remove string from response

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var WebSocketServer = require('ws').Server
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
         console.log('Received from client: %s', message);
-        ws.send('Server received from client: ' + message + ' Sir');
+        ws.send('Server received from client: ' + message);
     });
  });
